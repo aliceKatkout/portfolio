78071ef910aa1772900decedf35f69ccf9c545b0
aliceKatkout <alice.katkout@gmail.com>
2021-01-06 17:32:49 +0100

    mise en page

---
+++ scripts/yeuzanim.js
@@ -0,0 +1,40 @@
+let delay = 100;
+let imgNumber = 0;
+let totalimgNumber = 9;
+let anim = [];
+let start = document.getElementById("cligne");
+let timer;
+
+for (i=0; i<totalimgNumber; i++){
+  anim[i]= new Image (2000, 815);
+  anim[i].src ="images/oeil"+ (i+1)+ ".png";
+}
+
+function switch2() {
+  document.oeilAnim.src = anim[imgNumber].src;
+  imgNumber++;
+  if (imgNumber >= totalimgNumber) {
+   imgNumber = 0;
+   clearInterval(timer);
+ }
+}
+
+function animate1() {
+  console.log("animate ok");
+  //  switch2();
+  timer= setInterval(switch2, delay);
+  //setTimeout(animate1, delay);
+}
+
+start.addEventListener("mouseover", function(event) {
+  console.log("j'ai vu");
+  animate1();
+});
+
+// let paint = document.getElementById("peinture");
+//
+//
+//
+// paint.addEventListener("click", function(event) {
+//   console.log("coucoupaint");
+// });
