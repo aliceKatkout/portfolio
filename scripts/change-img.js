6e34f0835971aca1d72b5f3312b08b6425eadc85
aliceKatkout <alice.katkout@gmail.com>
2021-01-07 15:54:25 +0100

    code page

---
+++ scripts/change-img.js
@@ -0,0 +1,15 @@
+let pico = document.getElementById("pico");
+let acro =  document.getElementById("acro");
+let epilepsy = document.getElementById("epilepsy");
+
+pico.addEventListener("mouseover", function(event) {
+  document.imgCode.src="./images/code/pico1.png"
+});
+
+acro.addEventListener("mouseover", function(event) {
+  document.imgCode.src="./images/code/acro1.png"
+});
+
+epilepsy.addEventListener("mouseover", function(event) {
+  document.imgCode.src="./images/code/epilepsy-avion.png"
+});
